49d4a48ed0f60c38da96b36f58575eedc4161141
Felipe Medeiros <felipenovaesmedeiros@gmail.com>
2016-09-27 10:08:01 -0300

    bloqueio de professores no model Users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,79 +14,114 @@ class UserController extends Controller
         $this->middleware('auth');
     }
     
-	public function index()
+	public function index(Request $request)
     {
-        $users = User::paginate(10);
-        return view('usuarios.show', ['users' => $users]);
+        if($request->user()->office == 2){
+            return view('reservas.show');
+        }
+        else{
+            $users = User::paginate(10);
+            return view('usuarios.show', ['users' => $users]);
+        }
     }
 
     public function validator(array $data)
     {
-        return Validator::make($data, [
-            'name' => 'required|max:255',
-            'email' => 'required|email|max:255|unique:users',
-            'password' => 'required|min:6|confirmed',
-            'cpf' => 'required|max:11|min:11|unique:users',
-            'office' => 'required'
-        ]);
+        if($request->user()->office == 2){
+            return view('reservas.show');
+        }
+        else{
+            return Validator::make($data, [
+                'name' => 'required|max:255',
+                'email' => 'required|email|max:255|unique:users',
+                'password' => 'required|min:6|confirmed',
+                'cpf' => 'required|max:11|min:11|unique:users',
+                'office' => 'required'
+            ]);
+        }
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        if($request->user()->office == 2){
+            return view('reservas.show');
+        }
+        else{
                 return view('usuarios.create');
+            }
     }
 
     public function store(Request $request)
     {
-        $user = User::create($request->all());
-
-        $new_password = $request->password;
-        $user         = User::findOrFail($user->id);
-
-        $user->fill([
-            'name'  => $request->name,
-            'email' => $request->email,
-        ]);
-
-        if (!empty($new_password)) {
-            $user->password = bcrypt($new_password);
+        if($request->user()->office == 2){
+            return view('reservas.show');
         }
+        else{
+            $user = User::create($request->all());
 
-        $user->save();
+            $new_password = $request->password;
+            $user         = User::findOrFail($user->id);
 
-        return redirect(route('user.index'));
+            $user->fill([
+                'name'  => $request->name,
+                'email' => $request->email,
+            ]);
+
+            if (!empty($new_password)) {
+                $user->password = bcrypt($new_password);
+            }
+
+            $user->save();
+
+            return redirect(route('user.index'));
+        }
     }
 
-    public function edit($id)
+    public function edit(Request $request, $id)
     {
-        $usuario = User::find($id);
-        return view('usuarios.edit', ['usuario' => $usuario]);
+        if($request->user()->office == 2){
+            return view('reservas.show');
+        }
+        else{
+            $usuario = User::find($id);
+            return view('usuarios.edit', ['usuario' => $usuario]);
+        }
     }
 
-    public function destroy($id)
+    public function destroy(Request $request, $id)
     {
-        $user = User::find($id);
-        $user->delete();
-        return redirect(route('user.index'));
+        if($request->user()->office == 2){
+            return view('reservas.show');
+        }
+        else{
+            $user = User::find($id);
+            $user->delete();
+            return redirect(route('user.index'));
+        }
     }
 
 
     public function update(Request $request)
     {
-        //dd($request->id);
-        $new_password = $request->password;
-        $user         = User::findOrFail($request->id);
-
-        $user->fill([
-            'office'  => $request->office,
-        ]);
-
-        if (!empty($new_password)) {
-            $user->password = bcrypt($new_password);
+        if($request->user()->office == 2){
+            return view('reservas.show');
         }
+        else{
+            //dd($request->id);
+            $new_password = $request->password;
+            $user         = User::findOrFail($request->id);
 
-        $user->save();
-        return redirect(route('user.index'));
+            $user->fill([
+                'office'  => $request->office,
+            ]);
+
+            if (!empty($new_password)) {
+                $user->password = bcrypt($new_password);
+            }
+
+            $user->save();
+            return redirect(route('user.index'));
+        }
     }
 
 }
